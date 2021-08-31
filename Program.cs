43160ad4214602d7034f0f8db1a48103076c27bf
Arthur Silva <arthurteixeira.guts@gmail.com>
2021-08-31 17:41:06 -0300

    Configurando ambiente

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Introducao_NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introdução ao C# .NET");
+        }
+    }
+}
