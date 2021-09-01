565e8a53584275a5d3bf52b761509fdc654152a0
arthur-art <arthurteixeira.guts@gmail.com>
2021-09-01 10:47:14 -0300

    Convertendo variaveis

--- Introducao-NET/Program.cs
+++ Introducao-NET/Program.cs
@@ -7,23 +7,41 @@ namespace Introducao_NET
         static void Main(string[] args)
         {
             //Laço de repetição e concatenacao com c#
-            /*int numCinco = 5;
+
+            /*
+            int numCinco = 5;
             for(int i=0; i<numCinco; i++)
             {
                 Console.WriteLine($"Praticando laço de repetição com dotnet, {i}");
-            }*/
+            }
+            */
 
 
             //Escrevendo e lendo valores
+
+            /*
             Console.WriteLine("Ola, qual a sua idade ?");
             string input = Console.ReadLine();
             Console.WriteLine($"A sua idade é {input}");
+            */
 
             //Tipos de variaveis
+
+            /*
             string name = "Arthur";
             int age = 25;
             double weight = 75.5;
             bool dev = true;
+            Console.WriteLine($"{name} { age} { weight} { dev}");
+            */
+
+            //Convertendo variaveis, Console.ReadLine sempre irá retornar uma string, convertendo seu valor para um int
+
+            /*
+            Console.WriteLine("Digite um numero");
+            int faveNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Numero digitado, {faveNumber}");
+            */
         }
     }
 }
