38c73f11bc8d57cad9fde04cbab551788cf0fe98
arthur-art <arthurteixeira.guts@gmail.com>
2021-09-01 09:38:51 -0300

    Trabalhando com WriteLine e ReadLine no C#

--- Introducao-NET/Program.cs
+++ Introducao-NET/Program.cs
@@ -6,11 +6,16 @@ namespace Introducao_NET
     {
         static void Main(string[] args)
         {
-            int numCinco = 5;
+            /*int numCinco = 5;
             for(int i=0; i<numCinco; i++)
             {
                 Console.WriteLine($"Praticando laço de repetição com dotnet, {i}");
-            }
+            }*/
+
+            Console.WriteLine("Ola, qual a sua idade ?");
+            string input = Console.ReadLine();
+            Console.WriteLine($"A sua idade é {input}");
+
         }
     }
 }
