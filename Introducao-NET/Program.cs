63353c9240275aeb4fe40ccd9775d015a82ea525
arthur-art <arthurteixeira.guts@gmail.com>
2021-09-01 11:28:30 -0300

    Trabalhando com tipo de dados e operadores aritmeticos no c#

--- Introducao-NET/Program.cs
+++ Introducao-NET/Program.cs
@@ -8,40 +8,23 @@ namespace Introducao_NET
         {
             //Laço de repetição e concatenacao com c#
 
-            /*
+            
             int numCinco = 5;
             for(int i=0; i<numCinco; i++)
             {
                 Console.WriteLine($"Praticando laço de repetição com dotnet, {i}");
             }
-            */
+            
 
 
             //Escrevendo e lendo valores
 
-            /*
+            
             Console.WriteLine("Ola, qual a sua idade ?");
             string input = Console.ReadLine();
             Console.WriteLine($"A sua idade é {input}");
-            */
+            
 
-            //Tipos de variaveis
-
-            /*
-            string name = "Arthur";
-            int age = 25;
-            double weight = 75.5;
-            bool dev = true;
-            Console.WriteLine($"{name} { age} { weight} { dev}");
-            */
-
-            //Convertendo variaveis, Console.ReadLine sempre irá retornar uma string, convertendo seu valor para um int
-
-            /*
-            Console.WriteLine("Digite um numero");
-            int faveNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Numero digitado, {faveNumber}");
-            */
         }
     }
 }
---
+++ TipoDeDadosEVariaveis/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace TipoDeDadosEVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tipos de variaveis
+
+            
+            string name = "Arthur";
+            int age = 25;
+            double weight = 75.5;
+            bool dev = true;
+            decimal money = 10500.65m;
+            Console.WriteLine($"{name} { age} { weight} { dev} {money}");
+            
+
+            //Convertendo variaveis, Console.ReadLine sempre irá retornar uma string, convertendo seu valor para um int
+
+            
+            Console.WriteLine("Digite um numero");
+            int faveNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Numero digitado, {faveNumber}");
+            
+
+
+            // Operadores aritméticos
+
+            // Your Age
+            int userAge = 30;
+
+            // Length of years on Jupiter (in Earth years)
+            double jupiterYears = 11.86;
+
+            // Age on Jupiter
+            double jupiterAge = userAge / jupiterYears;
+
+            // Time to Jupiter
+            double journeyToJupiter = 6.142466;
+
+            // New Age on Earth
+            double newEarthAge = userAge + journeyToJupiter;
+
+            // New Age on Jupiter
+            double newJupiterAge = newEarthAge / jupiterYears;
+
+            // Log calculations to console
+            Console.WriteLine(jupiterAge);
+            Console.WriteLine(newEarthAge);
+            Console.WriteLine(newJupiterAge);
+        }
+    }
+}
