86e5ce3553462d26d59e92efb02c59e4b1aeffcb
arthur-art <arthurteixeira.guts@gmail.com>
2021-09-30 15:25:37 -0300

    Estudando metodos no c#

---
+++ Metodos/DeclaracoesCondicionais/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DeclaracoesCondicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Metodos simples do C#
+
+            string msg = "Yabba dabba doo!";
+
+            Math.Min(10, 12);
+
+            Console.WriteLine(msg);
+
+            var firstLetter =  msg.Substring(0, 1);
+
+            Console.WriteLine(firstLetter);
+
+            // Metodos void 
+
+            static void VisitPlanets()
+            {
+                Console.WriteLine("Mars");
+            }
+            VisitPlanets();
+        }
+    }
+}
