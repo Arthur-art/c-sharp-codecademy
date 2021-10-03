25fd4824a72910dd033e382f7bd6c1ec98273795
arthur-art <arthurteixeira.guts@gmail.com>
2021-10-03 12:12:47 -0300

    Criando uma primeira Interface

---
+++ 6-Interfaces/6-Interfaces/IAutomobile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _6_Interfaces
+{
+    interface IInitInterface
+    {
+        string LicensePlate { get; }
+        double Speed { get; }
+        int Wheels { get; }
+        void Honk();
+    }
+}
---
+++ 6-Interfaces/6-Interfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _6_Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
