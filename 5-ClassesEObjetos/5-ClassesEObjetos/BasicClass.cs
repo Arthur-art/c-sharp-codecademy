914e74dfe22f98ed456261131201a49406fc41e7
arthur-art <arthurteixeira.guts@gmail.com>
2021-10-02 21:01:58 -0300

    Iniciando capitulo sobre classes e objetos no c#

---
+++ 5-ClassesEObjetos/5-ClassesEObjetos/BasicClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5_ClassesEObjetos
+{
+    class BasicClass
+    {
+        public string name;
+        public string job;
+        public int age;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
---
+++ 5-ClassesEObjetos/5-ClassesEObjetos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _5_ClassesEObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Classes no C#
+
+            BasicClass b = new BasicClass();
+
+            b.name = "Arthur";
+            b.job = "Dev";
+            b.age = 25;
+
+            Console.WriteLine(b.name);
+        }
+    }
+}
