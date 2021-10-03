df00c12692b87696b574f4e883f1360c3a2b3682
arthur-art <arthurteixeira.guts@gmail.com>
2021-10-03 11:57:35 -0300

    Finalizando capitulo sobre metodos staticos

--- 5-ClassesEObjetos/5-ClassesEObjetos/Program.cs
+++ 5-ClassesEObjetos/5-ClassesEObjetos/Program.cs
@@ -1,9 +1,26 @@
-﻿using System;
+﻿using StaticMembers;
+using System;
 
 namespace _5_ClassesEObjetos
 {
     class Program
     {
+
+
+        /*
+        Main()é um método da Programclasse.
+
+        public- O método pode ser chamado fora da Programclasse.
+
+        static- O método é chamado a partir do nome da classe: Program.Main().
+
+        void - O método significa não retorna nada.
+
+        string[] args- O método tem um parâmetro denominado args, que é uma matriz de strings.
+
+        Cada vez que executamos dotnet run, o Main()método é chamado. Podemos incluir argumentos na linha de comando,
+        como se dotnet run arg1 arg2 arg3isso fosse convertido em um array como argsparâmetro.
+         */
         static void Main(string[] args)
         {
 
@@ -33,6 +50,18 @@ namespace _5_ClassesEObjetos
             ClassConstructor cc = new ClassConstructor("Arthur");
 
             Console.WriteLine(cc.Age);
+
+            // Classes Estaticas
+
+            Console.WriteLine(Math.PI);
+
+            Console.WriteLine(Math.Abs(-32));
+
+            Forest f = new Forest("Congo", "Tropical");
+
+            f.Grow();
+
+            Console.WriteLine(Forest.TreeFacts);
         }
     }
 }
