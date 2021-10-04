64e48319a828ff5c8e1c27ef6000182d1b65913b
arthur-art <arthurteixeira.guts@gmail.com>
2021-10-04 17:49:00 -0300

    Introducao a Listas

---
+++ 10-Listas/10-Listas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10_Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> marathons = new List<double>();
+
+            marathons.Add(133.42);
+            marathons.Add(421.43);
+
+            foreach(double value in marathons)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
