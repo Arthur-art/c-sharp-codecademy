322b78a5b0eb675fe2eb11d59e22f5e2523e4032
arthur-art <arthurteixeira.guts@gmail.com>
2021-10-06 16:55:27 -0300

    Introducao a LINQ

---
+++ 11-LINQ/11-LINQ/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listString = new List<string> { "D. Va", "Lucio", "Mercy", "Soldier 76", "Pharah", "Reinhardt" };
+
+
+            foreach (string value in listString)
+            {
+                if(value.Length > 6)
+                {
+                    Console.WriteLine(value);
+                }
+            }
+
+            var listLinq = from value in listString
+                           where value.Length > 6
+                           select value;
+
+            Console.WriteLine(string.Join(",", listLinq));
+        }
+    }
+}
