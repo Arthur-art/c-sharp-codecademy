f5c9eac3bc3cf90295ca32d782337139c4df5ef0
arthur-art <arthurteixeira.guts@gmail.com>
2021-10-13 17:08:24 -0300

    Finalizando curso, praticando LINQ

--- 11-LINQ/11-LINQ/Program.cs
+++ 11-LINQ/11-LINQ/Program.cs
@@ -38,6 +38,55 @@ namespace _11_LINQ
                 .Select(value => $"A palavra {value} contem a letra a");
 
             Console.WriteLine(string.Join(", ", resultMethod));
+
+            // Sintaxe de consulta basica
+
+            var heroesWithI = from hero in listString
+                              where hero.Contains("i")
+                              select hero;
+            Console.WriteLine(string.Join(",", heroesWithI));
+
+            var heroesUnderscored = from hero in listString
+                                    select hero.Replace(" ", "_");
+
+            Console.WriteLine(string.Join(",", heroesUnderscored));
+
+            // Sintaxe de metodo basico : Where
+
+            IEnumerable<string> shortHeroes = listString.Where(hero => hero.Length < 8);
+
+            IEnumerable<string> heroeWithI = listString.Where(hero => hero.Contains("i"));
+
+            Console.WriteLine("shortHeroes: "+string.Join(",",shortHeroes));
+
+            // Sintaxe de metodo basico : Select
+
+            IEnumerable<string> listStringToUpperCase = listString.Select(value => value.ToUpper());
+            foreach(string value in listStringToUpperCase)
+            {
+                Console.WriteLine(value);
+            }
+
+            IEnumerable<string> listSelectWhere = listString
+                .Where(value => value.Contains("c"))
+                .Select(value => value.ToLower());
+                
+
+            foreach (string value in listSelectWhere)
+            {
+                Console.WriteLine(value);
+            }
+
+            // Praticando
+
+            string[] heroes = { "D. Va", "Lucio", "Mercy", "Soldier 76", "Pharah", "Reinhardt" };
+
+            IEnumerable<string> formatheroes = heroes.Select(value => $"Introducing...[{value}]!");
+
+            var consultList = from hero in heroes
+                              where hero.Contains(" ")
+                              select Array.IndexOf(heroes, hero);
+
         }
     }
 }
